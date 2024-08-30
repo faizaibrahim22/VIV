ff4a97ef4080b42a0b70f40a51267482ae6c0644
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-08-30 23:45:04 +0500

    first co

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        message: "Enter your firstnumber",
+        name: "firstnumber",
+        type: "number",
+    }, {
+        message: "Enter your secondnumber",
+        name: "secondnumber",
+        type: "number",
+    }, {
+        message: "enter your calculator number",
+        name: "operator",
+        type: "list",
+        choices: ["subtraction", "Addition", "Division", "multiplation"]
+    }
+]);
+if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "multiplation") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else {
+    console.log("invalid   number");
+}
